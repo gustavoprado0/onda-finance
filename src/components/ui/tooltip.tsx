d21eb90dc4a0ffd27fda142bb776f29c68dc4264
gustavo.prado <gustavopsz2002@gmail.com>
2026-03-31 21:29:53 -0300

    fix(ui): correct Slot usage by replacing Slot.Root with Slot component

--- src/components/ui/tooltip.tsx
+++ src/components/ui/tooltip.tsx
@@ -2,8 +2,8 @@
 
 import * as React from "react"
 import { Tooltip as TooltipPrimitive } from "radix-ui"
+import { cn } from "../../lib/utils"
 
-import { cn } from "@/lib/utils"
 
 function TooltipProvider({
   delayDuration = 0,
